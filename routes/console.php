c745d367cb2da3334aae3b0a13c46ffa9b7b799d
WEBDEVSSC <soportewebssc@gmail.com>
2025-10-08 10:24:06 -0600

    FIX | HORARIO DE RESPALDO DE DB CAMBIADO

--- routes/console.php
+++ routes/console.php
@@ -13,4 +13,4 @@ Schedule::command('felicitaciones:enviar')->dailyAt('10:00');
 
 Schedule::command('correo:firma-nomina-pendiente')->dailyAt('10:00');
 
-Schedule::command('backup:db')->dailyAt('01:00');
+Schedule::command('backup:db')->dailyAt('10:25');
