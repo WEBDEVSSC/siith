7e5a890a6398c9fbaed477388e79687e369c0c64
WEBDEVSSC <soportewebssc@gmail.com>
2026-02-19 12:55:22 -0600

    FEAT | CAMPO DE VIGENCIA AGREGADO EN MAIL DE RELACION DE ENVIADOS CUMPLEANOS

--- resources/views/email/felicitaciones-enviadas.blade.php
+++ resources/views/email/felicitaciones-enviadas.blade.php
@@ -10,6 +10,7 @@
                 <th>Email</th>
                 <th>Clues Adscripción</th>
                 <th>Nombre Clues Adscripción</th>
+                <th>Vigencia</th>
             </tr>
         </thead>
         <tbody>
@@ -18,7 +19,7 @@
                     <td>{{ $persona->nombre }} {{ $persona->apellido_paterno }} {{ $persona->apellido_materno }}</td>
                     <td>{{ $persona->email }}</td>
                     <td>{{ $persona->puesto->clues_adscripcion ?? 'N/A' }}</td>
-                    <td>{{ $persona->puesto->clues_adscripcion_nombre ?? 'N/A' }}</td>
+                    <td>{{ $persona->puesto->vigencia ?? 'N/A' }}</td>
                 </tr>
             @endforeach
         </tbody>
