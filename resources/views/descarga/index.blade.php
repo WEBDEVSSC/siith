033b334232c8c0a6b0c59be76ce8e64aa781c56c
WEBDEVSSC <soportewebssc@gmail.com>
2026-06-24 13:53:17 -0600

    🎨 FEAT | MEJORAS VISUALES EN EL PANEL DE REPORTES

--- resources/views/descarga/index.blade.php
+++ resources/views/descarga/index.blade.php
@@ -3,11 +3,35 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1><strong>Descargas</strong></h1>
 @stop
 
 @section('content')
-    <a href="{{ route('descargarArchivos')}}">DESCARGAR</a>
+
+    <div class="card">
+        <div class="card-body">
+
+            <div class="row">
+
+                    <div class="col-md-2">
+                        <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>Académicos</h3>
+
+                            <p>Archivo ZIP</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                        <a href="{{ route('descargarArchivos') }}" class="small-box-footer">Descargar <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+            </div>
+
+        </div>
+    </div>
+
+    
 @stop
 
 @section('css')
