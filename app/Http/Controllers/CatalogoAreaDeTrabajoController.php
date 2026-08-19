f33c2ccc21a24f998a447b3395f43dd70d21f19b
WEBDEVSSC <soportewebssc@gmail.com>
2026-08-19 10:33:44 -0600

    ✨ FEAT | AREA DE TRABAJAO SETTINGS ORDENADA POR ORDEN ALFABETICO 2

--- app/Http/Controllers/CatalogoAreaDeTrabajoController.php
+++ app/Http/Controllers/CatalogoAreaDeTrabajoController.php
@@ -10,7 +10,7 @@ class CatalogoAreaDeTrabajoController extends Controller
     //
     public function indexAreaDeTrabajo()
     {
-        $areasTrabajo = AreaTrabajo::orderNy('area_trabajo', 'ASC');
+        $areasTrabajo = AreaTrabajo::orderBy('area_trabajo', 'ASC');
 
         return view('settings.areas-trabajo.index', compact('areasTrabajo'));
     }
